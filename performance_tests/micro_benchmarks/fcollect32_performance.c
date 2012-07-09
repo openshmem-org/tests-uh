8ee0786da6b22e8ebf0ad7c62bbe7e1abff5d76c
Ramachandra CN <ram@shark.pstl.uh.edu>
2012-07-08 19:03:24 -0500

    Use 2 pSync arrays to synchronise consequent collectives Remove redundant pSync initialization

--- performance_tests/micro_benchmarks/fcollect32_performance.c
+++ performance_tests/micro_benchmarks/fcollect32_performance.c
@@ -40,7 +40,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <shmem.h>
-static long pSync[_SHMEM_BCAST_SYNC_SIZE];
+static long pSyncA[_SHMEM_BCAST_SYNC_SIZE];
+static long pSyncB[_SHMEM_BCAST_SYNC_SIZE];
 
 #define N_ELEMENTS 4
   int
@@ -57,10 +58,6 @@ main(void)
   me = _my_pe();
   npes = _num_pes();
 
-  for (i = 0; i < _SHMEM_BCAST_SYNC_SIZE; i += 1) {
-    pSync[i] = _SHMEM_SYNC_VALUE;
-  }
-
   source = (int *) shmalloc( N_ELEMENTS * sizeof(*source) );
 
   time_taken = 0;
@@ -73,7 +70,8 @@ main(void)
     target[i] = -90;
   }
   for (i = 0; i < _SHMEM_BCAST_SYNC_SIZE; i += 1) {
-    pSync[i] = _SHMEM_SYNC_VALUE;
+    pSyncA[i] = _SHMEM_SYNC_VALUE;
+    pSyncB[i] = _SHMEM_SYNC_VALUE;
   }
   shmem_barrier_all();
 
@@ -82,7 +80,12 @@ main(void)
 
     start_time = (start.tv_sec * 1000000.0) + start.tv_usec;
 
-    shmem_fcollect32(target, source, N_ELEMENTS, 0, 0, npes, pSync);
+    /* alternate between 2 pSync arrays to synchronize
+     * consequent collectives of even and odd iterations */
+    if(i % 2)
+      shmem_fcollect32(target, source, N_ELEMENTS, 0, 0, npes, pSyncA);
+    else
+      shmem_fcollect32(target, source, N_ELEMENTS, 0, 0, npes, pSyncB);
 
     gettimeofday(&end, NULL);
 
