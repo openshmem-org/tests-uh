aca2b06660261d7c10f9c678d6ea6dfbac993a4b
Swaroop Pophale <swaroop@shark.pstl.uh.edu>
2012-09-17 13:20:40 -0500

    adding tests for strided broadcast calls with root !=0

--- feature_tests/C/test_shmem_broadcast.c
+++ feature_tests/C/test_shmem_broadcast.c
@@ -137,5 +137,82 @@ main(void)
   else{
     printf("Number of PEs must be > 1 to test broadcast, test skipped\n");
   }
-  return 0;
+  success32 = 0;
+  success64 = 0;
+
+  if(npes > 3){
+    for (i = 0; i < _SHMEM_BCAST_SYNC_SIZE; i += 1) {
+      pSync[i] = _SHMEM_SYNC_VALUE;
+    }
+
+    /*Test strided shmem_broadcast32*/
+    src = (int *) shmalloc( npes * sizeof(*src) );
+    for (i = 0; i < npes; i += 1) {
+      src[i] = i + 1;
+    }
+
+    targ = (int *) shmalloc( npes * sizeof(*targ) );
+    for (i = 0; i < npes; i += 1) {
+      targ[i] = -999;
+    }
+
+
+    shmem_barrier_all();
+
+    shmem_broadcast32(targ, src, npes, 0, 1, 1, npes/2, pSync);
+
+    shmem_barrier_all();
+
+    if(me == 3){
+      for (i = 0; i < npes; i++) {
+        if( targ[i] != (i+1))
+          success32=1;
+      }
+      if(success32==1)
+        printf("Test strided shmem_broadcast32: Failed\n");
+      else
+        printf("Test strided shmem_broadcast32: Passed\n");
+    }
+
+    shmem_barrier_all();
+
+    /*Test strided shmem_broadcast64*/
+
+    source = (long *) shmalloc( npes * sizeof(*source) );
+    for (i = 0; i < npes; i += 1) {
+      source[i] = i + 1;
+    }
+
+    target = (long *) shmalloc( npes * sizeof(*target) );
+    for (i = 0; i < npes; i += 1) {
+      target[i] = -999;
+    }
+
+
+    shmem_barrier_all();
+
+    shmem_broadcast64(target, source, npes, 0, 1, 1, npes/2, pSync);
+
+    shmem_barrier_all();
+
+    if(me == 3){
+      for (i = 0; i < npes; i++) {
+        if( target[i] != (i+1))
+          success64=1;
+      }
+      if(success64==1)
+        printf("Test strided shmem_broadcast64: Failed\n");
+      else
+        printf("Test strided shmem_broadcast64: Passed\n");
+    }
+
+    shfree(targ);
+    shfree(src);
+    shfree(target);
+    shfree(source);
+  }
+  else{
+    printf("Number of PEs must be > 3 to test broadcast, test skipped\n");
+    return 0;
+  }
 }
